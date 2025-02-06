f13ff715989287346d9bdb914393c7503e188fd2
Vikram Maurya <imvikrammaurya@outlook.com>
2025-02-06 17:25:36 +0530

    second commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var menuIcon = document.querySelector(".menu-icon")
+var sidebar = document.querySelector(".sidebar")
+var Container = document.querySelector(".container")
+
+menuIcon.onclick = function(){
+    sidebar.classList.toggle("small-sidebar");
+    Container.classList.toggle("large-container");
+}
+
